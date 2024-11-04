91fbe282d7a84701d5cbd6497c85ea6ac0a3f36e
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-04 14:03:19 +0530

    Update start.py

--- RAUSHAN/start.py
+++ RAUSHAN/start.py
@@ -12,16 +12,16 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_photo(
         chat_id=msg.chat.id,
-        photo="https://files.catbox.moe/6ksft0.jpg",
+        photo="https://envs.sh/Amn.jpg",
         caption=f"""**❍ » ʜᴇʏ  {msg.from_user.mention}  ✤,
 
-❍ » ɪ ᴀᴍ{me2},
+❍ » ɪ ᴀᴍ {me2},
 
 ❍ » A sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ.
 
 ❍ » ᴘʟᴇᴀꜱᴇ ᴄʜᴏᴏꜱᴇ ᴛʜᴇ ᴘʏᴛʜᴏɴ ʟɪʙʀᴀʀʏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ꜰᴏʀ.
 
-❍ » ɪғ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ, ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ: [ᬃ꯭ ⃪꯭ ꯭⁢⁣⁤⁣⁣⁢⁣⁤⁢⁤⁣⁢⁤⁣⁤᪳᪳🇷꯭𝚰𝛅꯭꯭ʜ꯭֟፝፝֟ᴜ ꯭˟༗꯭»꯭݅݅݅݅𓆪༐](tg://user?id={OWNER_ID}) ** """,
+❍ » ɪғ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ, ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ: [ᬃ꯭ ⃪꯭ ꯭⁢⁣⁤⁣⁣⁢⁣⁤⁢⁤⁣⁢⁤⁣⁤᪳᪳🇷꯭𝚰𝛅꯭꯭ʜ꯭֟፝፝֟ᴜ ꯭˟༗꯭»꯭݅݅݅݅𓆪༐](t.me/rishu1286) ** """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
