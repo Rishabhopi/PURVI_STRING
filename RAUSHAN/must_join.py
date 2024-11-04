c6ae5f4257415147855a58deb180993bc19363b0
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-04 13:59:37 +0530

    Update must_join.py

--- RAUSHAN/must_join.py
+++ RAUSHAN/must_join.py
@@ -20,14 +20,14 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://envs.sh/Amn.jpg", caption=f"» ғɪʀsᴛʟʏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ғᴀᴍɪʟʏ ᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ᴏғғɪᴄᴇ . \n\nᴀғᴛᴇʀ ᴊᴏɪɴ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
+                    photo="https://envs.sh/jeg.jpg", caption=f"» ғɪʀsᴛʟʏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ᴏᴜʀ ғᴀᴍɪʟʏ \n\nᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ  \n\nᴀғᴛᴇʀ ᴊᴏɪɴ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
                                 InlineKeyboardButton(" ᴏғғɪᴄᴇ ", url=link),
                             ],
                             [
-                                InlineKeyboardButton(" ᴏғғɪᴄᴇ ", url="https://t.me/UR_RISHU_143"),
+                                InlineKeyboardButton(" ᴄʜᴀɴɴᴇʟ ", url="https://t.me/UR_RISHU_143"),
                             ]
                         ]
                     )
