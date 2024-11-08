b330d271105dac76320359ad3d969351284a57a9
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-08 11:12:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID","5738579437"))
 
-MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("mongodb+srv://aradhyamusicbot:2QWdrmNlRVYovcVq@cluster0.ekjld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = getenv("MUST_JOIN","Nenobots")
